2dea5c892105d4dfda3c23661a325404a061b5ca
prasad-bigdp <bigdp585@gmail.com>
2024-04-22 12:37:47 +0530

    update

---
+++ JS/code/first/script.js
@@ -0,0 +1,15 @@
+// Take two numbers , add and print their result in console and alert
+
+// var a = +prompt("enter a value")
+// var b = +prompt("enter a value")
+// c = a + b
+// console.log(c)
+
+// data types demo
+var d = 25.36555;
+var e = 35.443;
+console.log(typeof (d)) // number data type is used for numbers including decimals
+console.log(e.toFixed(2))
+console.log(e.toPrecision(3))
+console.log(e.toString(16))
+
