608803a5fb642638c60ee75b98aa60c1dcddf865
prasad-bigdp <bigdp585@gmail.com>
2024-04-26 13:40:57 +0530

    update

---
+++ JS/code/Functions/script.js
@@ -0,0 +1,27 @@
+function prasadPrint ()//declaration -- function keyword, name , parameters
+{
+    console.log("print prasad");//definition
+}
+prasadPrint() // call
+/* function to add two numbers */
+function add (a, b)
+{
+console.log(`addition is ${a+b}`)
+}
+add("hi","bye")
+/* write a simple function to add two strings*/
+/* callback functions -- one function inside the parameter of another function*/
+
+var xyz= function ()//anonymous function
+{
+    console.log("I'm bad")
+} // function expression
+console.log(bad)
+function mad (a,something)
+{
+   console.log(something)
+    console.log("i'm mad")
+}
+mad(2, xyz)
+//bad is called callback function
+//mad is called Higher order function
--- JS/code/loops/loops.js
+++ JS/code/loops/loops.js
@@ -1,14 +1,84 @@
-/* print number from 1 to 10 using while */
-var a = 0;
-while (a <= 10)
+// /* print number from 1 to 10 using while */
+// var a = 0;
+// while (a <= 10)
+// {
+//     console.log(a);
+//     a+=2;
+// }
+// /* implement same using do while */
+// var b = 11;
+// do
+// {
+//     console.log(b);
+//     b++;
+// } while (b <= 10)
+
+// /*implement 1 to 20 in reverse order odd number */
+// var n = 19
+// while (n >= 1) {
+// 	console.log(n)
+// 	n -= 2
+// }
+// /*printing all letters in name*/
+// var s = "Garima"
+// var n = s.length-1
+// while (n >= 0) {
+// 	console.log(s.charAt(n))
+// 	n--
+// }
+// /*count no of vowls and consonents */
+// var s='GARIMA'.toLowerCase()
+// var n=0
+// var vow=0
+// var cons=0
+
+// while(n<=s.length-1)
+// {
+// if((s.charAt(n))=='a'||(s.charAt(n))=='e'||(s.charAt(n))=='i'||(s.charAt(n))=='o'||(s.charAt(n))=='u')
+// vow++;
+// n++
+// }
+// console.log(`vowels in Garima are ${vow}`)
+// console.log(`Consonants in Garima are ${cons}`)
+
+//for loop
+//standard for
+for (var i = 25; i >=1;i-=2)
 {
-    console.log(a);
-    a+=2;
+    console.log(i)
 }
-/* implement same using do while */
-var b = 11;
-do
+// for loops usually used to iterate array. array is a collection of elements
+var arr = [2, 3, 5, 8]
+
+
+for (var a = 0; a < arr.length; a++)
 {
-    console.log(b);
-    b++;
-} while (b<=10)
+    console.log(arr[a])
+}
+// for-- of
+//to iterate array elements you can use for--of
+var arr = ["welcome", "everyone", "to", "JS"]
+
+for (var a of arr)
+{
+    console.log(a)
+}
+
+// for ..in
+
+for (var a in arr)
+{
+    console.log(a)
+}
+    // for--in for objects, for--of array..
+
+
+
+
+
+
+
+
+
+
+
